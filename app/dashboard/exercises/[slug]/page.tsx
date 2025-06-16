165830cd05173989c004a98986441fbd93d889bd
Angel <angelhodar76@gmail.com>
2025-06-16 23:38:49 +0200

    Removed async warning in params

--- app/dashboard/exercises/[slug]/page.tsx
+++ app/dashboard/exercises/[slug]/page.tsx
@@ -2,11 +2,12 @@ import { notFound } from "next/navigation"
 import { getExerciseFromRegistry, exerciseRegistry } from "@/app/registry/exercises"
 import { ExerciseRunner } from "@/components/exercise-runner"
 
-export default function DynamicExercisePage({
-  params,
-}: {
-  params: { slug: string }
-}) {
+export default async function DynamicExercisePage(
+  props: {
+    params: Promise<{ slug: string }>
+  }
+) {
+  const params = await props.params;
   if (!getExerciseFromRegistry(params.slug)) notFound()
 
   return <ExerciseRunner slug={params.slug} />

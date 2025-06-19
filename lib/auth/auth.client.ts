f3af257a5d0709ac3ba31006da588988421c371c
Angel <angelhodar76@gmail.com>
2025-06-19 19:18:41 +0200

    Added proper env var for auth base url

--- lib/auth/auth.client.ts
+++ lib/auth/auth.client.ts
@@ -1,7 +1,16 @@
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from "better-auth/react";
+
+// NEXT_PUBLIC_VERCEL_URL
+// NEXT_PUBLIC_VERCEL_BRANCH_URL
+
+// TODO: Add a check for the environment to select the proper env var in case of preview deployments. For now, we're using the production URL.
+
+const baseURL = process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
+  : "http://localhost:3000";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
-})
+  baseURL,
+});
 
-export const { signIn, signUp, signOut, useSession } = authClient
+export const { signIn, signUp, signOut, useSession } = authClient;

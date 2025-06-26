c1b13c5944b97dbc1b51a795109537a697ed6f86
Angel <angelhodar76@gmail.com>
2025-06-26 17:11:34 +0200

    URL fix

--- lib/auth/auth.client.ts
+++ lib/auth/auth.client.ts
@@ -1,9 +1,20 @@
 import { createAuthClient } from "better-auth/react";
 
+const getBaseURL = () => {
+  if (!process.env.NEXT_PUBLIC_VERCEL_ENV) {
+    return "http://localhost:3000";
+  }
+
+  const url =
+    process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+      ? process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+      : process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL;
+
+  return `https://${url}`;
+};
+
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-    : "http://localhost:3000",
+  baseURL: getBaseURL(),
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;

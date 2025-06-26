ab1d8ef021b79a7baa056108d9e1bb9d792ab514
Angel <angelhodar76@gmail.com>
2025-06-26 15:46:54 +0200

    Allow proper domain url handling for auth in preview envs

--- lib/auth/auth.client.ts
+++ lib/auth/auth.client.ts
@@ -1,13 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 
-// NEXT_PUBLIC_VERCEL_URL
-// NEXT_PUBLIC_VERCEL_BRANCH_URL
-
-// TODO: Add a check for the environment to select the proper env var in case of preview deployments. For now, we're using the production URL.
-
-const baseURL = process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
-  : "http://localhost:3000";
+const baseURL = process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000";
 
 export const authClient = createAuthClient({
   baseURL,

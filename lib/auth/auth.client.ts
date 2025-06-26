ff4c2baf8844971122e95f8babc7b58d0dd2a813
Angel <angelhodar76@gmail.com>
2025-06-26 15:54:07 +0200

    Better env detectionn for deployment url

--- lib/auth/auth.client.ts
+++ lib/auth/auth.client.ts
@@ -1,11 +1,20 @@
 import { createAuthClient } from "better-auth/react";
 
-const baseURL = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : "http://localhost:3000";
+const getBaseURL = () => {
+  if (!process.env.VERCEL) {
+    return "http://localhost:3000";
+  }
+
+  const url =
+    process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+      ? process.env.NEXT_PUBLIC_VERCEL_URL
+      : process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL;
+
+  return `https://${url}`;
+};
 
 export const authClient = createAuthClient({
-  baseURL,
+  baseURL: getBaseURL(),
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;

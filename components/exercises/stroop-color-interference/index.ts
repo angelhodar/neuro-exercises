767bc638cc0dd72eb0f7eb4e8bcd229a06564e69
Angel <angelhodar76@gmail.com>
2025-06-30 18:19:45 +0200

    feat(stroop-color-interference): añadir components/exercises/stroop-color-interference/index.ts

---
+++ components/exercises/stroop-color-interference/index.ts
@@ -0,0 +1,8 @@
+export { StroopColorInterferenceExercise } from "./stroop-color-interference-exercise";
+export { StroopColorInterferenceResults } from "./stroop-color-interference-results";
+export { StroopColorInterferenceConfigFields } from "./stroop-color-interference-config-form";
+export {
+  stroopColorInterferenceConfigSchema,
+  defaultStroopColorInterferenceConfig,
+  stroopColorInterferencePresets,
+} from "./stroop-color-interference-schema";

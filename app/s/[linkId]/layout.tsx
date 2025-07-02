5063038ac99ccff8949b8d3bae2e841d80484606
Angel <angelhodar76@gmail.com>
2025-07-02 19:42:18 +0200

    Removed old linkId paths

--- app/s/[linkId]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export default function ExerciseSessionLayout({ children }: PropsWithChildren) {
-  return (
-    <div className="flex flex-col min-h-screen bg-blue-50">
-      <main className="flex-1">{children}</main>
-    </div>
-  );
-}
